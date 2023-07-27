4ab96c25c25e5d8f98378995b8698c48a096373f
Eros000 <e.ghilardi@hotmail.it>
2023-07-27 16:18:45 +0200

    working on main

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -13,9 +13,492 @@ export default {
 </script>
 
 <template>
+
+  <main>
+
+    <div class="main-jumbotron">
+
+      <div>
+        
+        <h2>
+          Udemy Affiliate Sales
+        </h2>
+
+        <p>
+          Monetize your audience and attract new customers with Udemy!
+        </p>
+
+      </div>
+
+      <img src="../assets/slide-1.jpg" alt="">
+    </div>
+
+
+    <div class="main-pop-dev-courses">
+
+      <h3>
+        Popular Development Courses
+      </h3>
+
+      <div class="cards">
+
+        <div class="card">
+
+          <img class="card-top-img" src="../assets/895786_7b4b_2-272x161.jpg" alt="">
+
+          <h5>
+            Development>
+          </h5>
+
+          <p>
+            The Complete iOs 10 & Swift 3 Developer Course
+          </p>
+
+          <hr>
+
+          <div>
+
+            <div class="stars">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/staremptyl.svg" alt="">
+            </div>
+
+            <div class="price">
+
+              <small class="small-1">
+                $199.99
+              </small>
+
+              <small>
+                $100
+              </small>
+
+            </div>
+
+          </div>
+
+        </div>
+
+        <div class="card">
+
+          <img class="card-top-img" src="../assets/246154_d8b0_3-272x161.jpg" alt="">
+
+          <h5>
+            Development>
+          </h5>
+
+          <p>
+            Web deisgn for Beginners: Real World Coding in HTML & CSS
+          </p>
+
+          <hr>
+
+          <div>
+
+            <div class="stars">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/staremptyl.svg" alt="">
+            </div>
+
+            <div class="price">
+
+              <small class="small-1">
+                $129.99
+              </small>
+
+              <small>
+                $65
+              </small>
+
+            </div>
+
+          </div>
+
+        </div>
+
+        <div class="card">
+
+          <img class="card-top-img" src="../assets/752950_b773-272x161.jpg" alt="">
+
+          <h5>
+            Development>
+          </h5>
+
+          <p>
+            GitHub Ultimate: Master Git and GitHub - Beginner to Expert
+          </p>
+
+          <hr>
+
+          <div>
+
+            <div class="stars">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/staremptyl.svg" alt="">
+            </div>
+
+            <div class="price">
+
+              <small class="small-1">
+                $99.99
+              </small>
+
+              <small>
+                $50
+              </small>
+
+            </div>
+
+          </div>
+
+        </div>
+
+        <div class="card">
+
+          <img class="card-top-img" src="../assets/1253188_58f7_2-272x161.jpg" alt="">
+
+          <h5>
+            Development>
+          </h5>
+
+          <p>
+            The Complete iOs 11 & Swift Developer Course - Build 20 Apps
+          </p>
+
+          <hr>
+
+          <div>
+
+            <div class="stars">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/staremptyl.svg" alt="">
+            </div>
+
+            <div class="price">
+
+              <small class="small-1">
+                $199.99
+              </small>
+
+              <small>
+                $100
+              </small>
+
+            </div>
+
+          </div>
+
+        </div>
+
+        <div class="card">
+
+          <img class="card-top-img" src="../assets/919872_ed54_6-272x161.jpg" alt="">
+
+          <h5>
+            Development>
+          </h5>
+
+          <p>
+            Android Java Masterclass - Become an App Developer
+          </p>
+
+          <hr>
+
+          <div>
+
+            <div class="stars">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/staremptyl.svg" alt="">
+            </div>
+
+            <div class="price">
+
+              <small class="small-1">
+                $99.99
+              </small>
+
+              <small>
+                $50
+              </small>
+
+            </div>
+
+          </div>
+
+        </div>
+
+        <div class="card">
+
+          <img class="card-top-img" src="../assets/951684_9c1a_2-272x161.jpg" alt="">
+
+          <h5>
+            Development>
+          </h5>
+
+          <p>
+            Xamarin Forms: Build Native Cross-platform Apps with C#
+          </p>
+
+          <hr>
+
+          <div>
+
+            <div class="stars">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/staremptyl.svg" alt="">
+            </div>
+
+            <div class="price">
+
+              <small class="small-1">
+                $189.99
+              </small>
+
+              <small>
+                $95
+              </small>
+
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+    <div class="main-jumbotron-2">
+
+      <div>
+
+        <h2>
+          Limitless Learning, more possibilities
+        </h2>
+
+        <p>
+          Online courses open the opportunity for learning to almost anyone, regardless of their scheduling commitments.
+        </p>
+
+        <button>
+
+          <a href="">READ MORE</a>
+
+        </button>
+
+      </div>
+      
+    </div>
+
+    <div class="recent-courses">
+      
+    </div>
+
+  </main>
   
 </template>
 
 <style scoped lang="scss">
+
+main{
+  margin-bottom: 100px;
+
+  .main-jumbotron{
+    margin-top: 30px;
+    height: 350px;
+    background-color: #FF4450;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+
+
+    div{
+      width: 550px;
+      height: 200px;
+      color: white;
+      text-align: left;
+      line-height: 10px;
+      margin-left: 250px;
+      
+
+      h2{
+        font-size: 48px;
+      }
+
+      p{
+        font-size: 24px;
+        line-height: 25px;
+      }
+    }
+
+
+    img{
+      width: 900px;
+      height: 350px;
+      clip-path: polygon(0 0, 504px 1110px, 1070px 130px, 905px 0px);
+      
+    }
+
+  }
+
+  .main-pop-dev-courses{
+    padding-top: 80px;
+    max-width: 1200px;
+    margin: 0 auto;
+
+    h3{
+
+    }
+
+    .cards{
+      margin-top: 50px;
+      display: flex;
+      flex-wrap: wrap;
+
+
+      .card{
+        width: calc((100% / 6) - 22px);
+        margin: 0 10px;
+        height: 250px;
+        border: 1px solid rgb(172, 172, 172);
+
+
+
+        .card-top-img{
+          width: 100%;
+
+        }
+
+        h5{
+          padding-left: 15px;
+          margin: 0 0;
+          margin-top: 10px;
+          color: lightgrey;
+
+        }
+
+        p{
+          font-size: 11px;
+          padding-left: 15px;
+          padding-right: 15px;
+          margin: 0 0;
+          margin-top: 10px;
+
+
+        }
+
+        hr{
+          margin-left: 15px;
+          margin-right: 15px;
+
+        }
+
+        div{
+          margin-top: 5px;
+          display: flex;
+          justify-content: space-between;
+          
+          
+
+          .stars{
+            padding-left: 15px;
+
+            img{
+              width: 10px;
+            }
+          }
+
+          .price{
+            padding-right: 15px;
+            margin-left: 20px;
+            display: flex;
+            flex-direction: column;
+            
+
+            small{
+              font-size: 11px;
+              
+            }
+
+            .small-1{
+              color: lightgrey;
+              text-decoration: line-through;
+            }
+          }
+        }
+      }
+    }
+  }
+
+
+  .main-jumbotron-2{
+    margin-top: 100px;
+    width: 100vw;
+    height: 600px;
+    background-image: url('../assets/bg.png');
+    background-size: contain;
+    background-repeat: no-repeat;
+
+
+
+    div{
+      padding-top: 120px;
+      padding-left: 400px;
+      padding-right: 900px;
+
+
+      h2{
+        font-size: 44px;
+        margin-bottom: 0px;
+
+      }
+
+      p{
+        
+
+      }
+
+
+      button{
+        background-color: #FF4450;
+        width: 100px;
+        height: 45px;
+        border: none;
+        border-radius: 30px;
+        color: white;
+        margin-top: 30px;
+
+
+        a{
+          text-decoration: none;
+          color: white;
+          font-weight: 700;
+          font-size: 11px;
+          
+        }
+      }
+    }
+
+  }
+}
 
 </style>
