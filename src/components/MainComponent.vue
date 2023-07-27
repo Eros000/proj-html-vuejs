ed6114a4121837cc4aac01e57daa327a287399d9
Eros000 <e.ghilardi@hotmail.it>
2023-07-27 17:12:25 +0200

    4th main-section done

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -309,6 +309,568 @@ export default {
     </div>
 
     <div class="recent-courses">
+
+      <h2>
+        Recents Courses
+      </h2>
+
+      <ul>
+
+        <li>
+
+          <a href="">
+            All Categories
+          </a>
+
+        </li>
+
+        <li>
+
+          <a href="">
+            Business
+          </a>
+
+        </li>
+
+        <li>
+
+          <a href="">
+            Design
+          </a>
+
+        </li>
+
+        <li>
+
+          <a href="">
+            Development
+          </a>
+
+        </li>
+
+        <li>
+
+          <a href="">
+            IT & Software
+          </a>
+
+        </li>
+
+        <li>
+
+          <a href="">
+            Lifestyle
+          </a>
+
+        </li>
+
+        <li>
+
+          <a href="">
+            Marketing
+          </a>
+
+        </li>
+
+        <li>
+
+          <a href="">
+            Office Productivity
+          </a>
+
+        </li>
+
+        
+        
+      </ul>
+
+      <div class="cards">
+
+        <div class="card">
+
+          <img class="card-top-img" src="../assets/895786_7b4b_2-272x161.jpg" alt="">
+
+          <h5>
+            Development>
+          </h5>
+
+          <p>
+            The Complete iOs 10 & Swift 3 Developer Course
+          </p>
+
+          <hr>
+
+          <div>
+
+            <div class="stars">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/staremptyl.svg" alt="">
+            </div>
+
+            <div class="price">
+
+              <small class="small-1">
+                $199.99
+              </small>
+
+              <small>
+                $100
+              </small>
+
+            </div>
+
+          </div>
+
+        </div>
+
+        <div class="card">
+
+          <img class="card-top-img" src="../assets/246154_d8b0_3-272x161.jpg" alt="">
+
+          <h5>
+            Development>
+          </h5>
+
+          <p>
+            Web deisgn for Beginners: Real World Coding in HTML & CSS
+          </p>
+
+          <hr>
+
+          <div>
+
+            <div class="stars">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/staremptyl.svg" alt="">
+            </div>
+
+            <div class="price">
+
+              <small class="small-1">
+                $129.99
+              </small>
+
+              <small>
+                $65
+              </small>
+
+            </div>
+
+          </div>
+
+        </div>
+
+        <div class="card">
+
+          <img class="card-top-img" src="../assets/752950_b773-272x161.jpg" alt="">
+
+          <h5>
+            Development>
+          </h5>
+
+          <p>
+            GitHub Ultimate: Master Git and GitHub - Beginner to Expert
+          </p>
+
+          <hr>
+
+          <div>
+
+            <div class="stars">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/staremptyl.svg" alt="">
+            </div>
+
+            <div class="price">
+
+              <small class="small-1">
+                $99.99
+              </small>
+
+              <small>
+                $50
+              </small>
+
+            </div>
+
+          </div>
+
+        </div>
+
+        <div class="card">
+
+          <img class="card-top-img" src="../assets/1253188_58f7_2-272x161.jpg" alt="">
+
+          <h5>
+            Development>
+          </h5>
+
+          <p>
+            The Complete iOs 11 & Swift Developer Course - Build 20 Apps
+          </p>
+
+          <hr>
+
+          <div>
+
+            <div class="stars">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/staremptyl.svg" alt="">
+            </div>
+
+            <div class="price">
+
+              <small class="small-1">
+                $199.99
+              </small>
+
+              <small>
+                $100
+              </small>
+
+            </div>
+
+          </div>
+
+        </div>
+
+        <div class="card">
+
+          <img class="card-top-img" src="../assets/919872_ed54_6-272x161.jpg" alt="">
+
+          <h5>
+            Development>
+          </h5>
+
+          <p>
+            Android Java Masterclass - Become an App Developer
+          </p>
+
+          <hr>
+
+          <div>
+
+            <div class="stars">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/staremptyl.svg" alt="">
+            </div>
+
+            <div class="price">
+
+              <small class="small-1">
+                $99.99
+              </small>
+
+              <small>
+                $50
+              </small>
+
+            </div>
+
+          </div>
+
+        </div>
+
+        <div class="card">
+
+          <img class="card-top-img" src="../assets/951684_9c1a_2-272x161.jpg" alt="">
+
+          <h5>
+            Development>
+          </h5>
+
+          <p>
+            Xamarin Forms: Build Native Cross-platform Apps with C#
+          </p>
+
+          <hr>
+
+          <div>
+
+            <div class="stars">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/staremptyl.svg" alt="">
+            </div>
+
+            <div class="price">
+
+              <small class="small-1">
+                $189.99
+              </small>
+
+              <small>
+                $95
+              </small>
+
+            </div>
+
+          </div>
+
+        </div>
+
+        <div class="card">
+
+          <img class="card-top-img" src="../assets/895786_7b4b_2-272x161.jpg" alt="">
+
+          <h5>
+            Development>
+          </h5>
+
+          <p>
+            The Complete iOs 10 & Swift 3 Developer Course
+          </p>
+
+          <hr>
+
+          <div>
+
+            <div class="stars">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/staremptyl.svg" alt="">
+            </div>
+
+            <div class="price">
+
+              <small class="small-1">
+                $199.99
+              </small>
+
+              <small>
+                $100
+              </small>
+
+            </div>
+
+          </div>
+
+        </div>
+
+        <div class="card">
+
+          <img class="card-top-img" src="../assets/246154_d8b0_3-272x161.jpg" alt="">
+
+          <h5>
+            Development>
+          </h5>
+
+          <p>
+            Web deisgn for Beginners: Real World Coding in HTML & CSS
+          </p>
+
+          <hr>
+
+          <div>
+
+            <div class="stars">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/staremptyl.svg" alt="">
+            </div>
+
+            <div class="price">
+
+              <small class="small-1">
+                $129.99
+              </small>
+
+              <small>
+                $65
+              </small>
+
+            </div>
+
+          </div>
+
+        </div>
+
+        <div class="card">
+
+          <img class="card-top-img" src="../assets/752950_b773-272x161.jpg" alt="">
+
+          <h5>
+            Development>
+          </h5>
+
+          <p>
+            GitHub Ultimate: Master Git and GitHub - Beginner to Expert
+          </p>
+
+          <hr>
+
+          <div>
+
+            <div class="stars">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/staremptyl.svg" alt="">
+            </div>
+
+            <div class="price">
+
+              <small class="small-1">
+                $99.99
+              </small>
+
+              <small>
+                $50
+              </small>
+
+            </div>
+
+          </div>
+
+        </div>
+
+        <div class="card">
+
+          <img class="card-top-img" src="../assets/1253188_58f7_2-272x161.jpg" alt="">
+
+          <h5>
+            Development>
+          </h5>
+
+          <p>
+            The Complete iOs 11 & Swift Developer Course - Build 20 Apps
+          </p>
+
+          <hr>
+
+          <div>
+
+            <div class="stars">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/staremptyl.svg" alt="">
+            </div>
+
+            <div class="price">
+
+              <small class="small-1">
+                $199.99
+              </small>
+
+              <small>
+                $100
+              </small>
+
+            </div>
+
+          </div>
+
+        </div>
+
+        <div class="card">
+
+          <img class="card-top-img" src="../assets/919872_ed54_6-272x161.jpg" alt="">
+
+          <h5>
+            Development>
+          </h5>
+
+          <p>
+            Android Java Masterclass - Become an App Developer
+          </p>
+
+          <hr>
+
+          <div>
+
+            <div class="stars">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/staremptyl.svg" alt="">
+            </div>
+
+            <div class="price">
+
+              <small class="small-1">
+                $99.99
+              </small>
+
+              <small>
+                $50
+              </small>
+
+            </div>
+
+          </div>
+
+        </div>
+
+        <div class="card">
+
+          <img class="card-top-img" src="../assets/951684_9c1a_2-272x161.jpg" alt="">
+
+          <h5>
+            Development>
+          </h5>
+
+          <p>
+            Xamarin Forms: Build Native Cross-platform Apps with C#
+          </p>
+
+          <hr>
+
+          <div>
+
+            <div class="stars">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/starfull.svg" alt="">
+              <img src="../assets/staremptyl.svg" alt="">
+            </div>
+
+            <div class="price">
+
+              <small class="small-1">
+                $189.99
+              </small>
+
+              <small>
+                $95
+              </small>
+
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+
+      <button>
+        SHOW ALL
+      </button>
       
     </div>
 
@@ -499,6 +1061,140 @@ main{
     }
 
   }
+
+
+  .recent-courses{
+    max-width: 1200px;
+    margin: 0 auto;
+    margin-top: 100px;
+    
+
+
+
+
+    h2{
+      text-align: center;
+
+    }
+
+    ul{
+      margin-top: 40px;
+      display: flex;
+      justify-content: center;
+
+      li{
+      list-style: none;
+
+        a{
+          text-decoration: none;
+          color: gray;
+          margin-left: 15px;
+          padding: 10px 5px;
+
+        }
+
+        a:hover{
+          background-color: rgb(226, 226, 226);
+          border-radius: 15px;
+          transition: 300ms;
+          
+        }
+      }
+    }
+
+    .cards{
+      margin-top: 50px;
+      display: flex;
+      flex-wrap: wrap;
+
+
+      .card{
+        width: calc((100% / 6) - 22px);
+        margin: 0 10px;
+        height: 250px;
+        border: 1px solid rgb(172, 172, 172);
+        margin-top: 20px;
+
+
+
+        .card-top-img{
+          width: 100%;
+
+        }
+
+        h5{
+          padding-left: 15px;
+          margin: 0 0;
+          margin-top: 10px;
+          color: lightgrey;
+
+        }
+
+        p{
+          font-size: 11px;
+          padding-left: 15px;
+          padding-right: 15px;
+          margin: 0 0;
+          margin-top: 10px;
+
+
+        }
+
+        hr{
+          margin-left: 15px;
+          margin-right: 15px;
+
+        }
+
+        div{
+          margin-top: 5px;
+          display: flex;
+          justify-content: space-between;
+          
+          
+
+          .stars{
+            padding-left: 15px;
+
+            img{
+              width: 10px;
+            }
+          }
+
+          .price{
+            padding-right: 15px;
+            margin-left: 20px;
+            display: flex;
+            flex-direction: column;
+            
+
+            small{
+              font-size: 11px;
+              
+            }
+
+            .small-1{
+              color: lightgrey;
+              text-decoration: line-through;
+            }
+          }
+        }
+      }
+    }
+
+    button{
+      background-color: #FF4450;
+      width: 100px;
+      height: 45px;
+      border: none;
+      border-radius: 30px;
+      color: white;
+      margin-top: 30px;
+      margin-left: 550px;
+    }
+  }
+
+
 }
 
 </style>
